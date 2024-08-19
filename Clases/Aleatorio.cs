7228f0f62e976fdc7693110b3665ac66217c234a
mzantana <zantana.miriam@gmail.com>
2024-08-19 17:56:01 -0600

    Construccion inicial, no se xdd

---
+++ Clases/Aleatorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaPrintRafa19_Agosto.Clases
+{
+    public class Aleatorio
+    {
+        public static int NumeroAleatorio(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        public int Valor { get; set; }
+        public int Posicion { get; set; }
+        public long Id { get; set; }
+        public Aleatorio() { }
+
+    }
+}
